6028834f53df4b7e4e5596743115b43d1e95b84f
Ethan Luna <ethanlunacado@gmail.com>
2024-11-05 11:39:26 -0600

    Refactor(): cell_agent.py refactor

--- agents/cell_agent.py
+++ agents/cell_agent.py
@@ -1,27 +1,28 @@
 from mesa import Agent
 
 class CellAgent(Agent):
+    STATES = {
+            0: "clear",
+            1: "droplets",
+            2: "gooed",
+    }
+        
+
     def __init__(self, pos, model, raw_walls):
         super().__init__(pos, model)
         self.pos = pos
         self.walls = self.parse_walls(raw_walls)  # Extraer las paredes de la celda
-
-        self.possible_states = {
-            0: "clear",
-            1: "droplets",
-            2: "gooed",
-        }
-        
+        self.state = CellAgent.STATES[0]
 
     #We process the walls info
     def parse_walls(self, raw_walls):
-        walls = {
+        WALLS = {
             "top": raw_walls[0] == "1",
             "left": raw_walls[1] == "1",
             "bottom": raw_walls[2] == "1",
             "right": raw_walls[3] == "1",
         }
-        return walls
+        return WALLS
 
     def set_state(self, state_key):
         if state_key in CellAgent.STATES:
