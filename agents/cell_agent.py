c010ed90af32a077e5b618427cb5f0e3c77929e4
Ethan Luna <ethanlunacado@gmail.com>
2024-11-05 10:36:39 -0600

    feat(): agregue un diccionario para la parte del contenido de las celdas

--- agents/cell_agent.py
+++ agents/cell_agent.py
@@ -4,15 +4,21 @@ class CellAgent(Agent):
     def __init__(self, pos, model, raw_walls):
         super().__init__(pos, model)
         self.pos = pos
-        self.state = "clear"  # Contenido de la celda
         self.walls = self.parse_walls(raw_walls)  # Extraer las paredes de la celda
 
+        self.possible_states = {
+            0: "clear",
+            1: "droplets",
+            2: "gooed",
+        }
+        
+
     #We process the walls info
-    def parse_walls(self, walls_code):
+    def parse_walls(self, raw_walls):
         walls = {
-            "top": walls_code[0] == "1",
-            "left": walls_code[1] == "1",
-            "bottom": walls_code[2] == "1",
-            "right": walls_code[3] == "1",
+            "top": raw_walls[0] == "1",
+            "left": raw_walls[1] == "1",
+            "bottom": raw_walls[2] == "1",
+            "right": raw_walls[3] == "1",
         }
         return walls
